9e0554a0b9ca6e7632a35f089cceb8740d68fcfd
Gordon-928 <94927083+Gordon-928@users.noreply.github.com>
2022-08-20 05:09:49 -0700

    Data Writing and GUI Improvement
    - Confirmed time stamp work
    - Removed "Calibration", sample 0 will be the calibration set
    - Updated buttons states to prevent double activation
    - Documentation is required

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -6,7 +6,6 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import serial
 import string
-#import pandas as pd
 import csv
 from datetime import datetime, time
 
@@ -44,9 +43,7 @@ canvas = FigureCanvasTkAgg(fig, master = root) #declare a canvas object
 canvas.get_tk_widget().pack(fill='x') #place the canvas in root
 canvas.draw()
 
-def gui_start():
-    global state_run
-    state_run = True
+
 
 def reset_array(a : tuple):
     x = ((np.array([])),) * len(a)
@@ -85,7 +82,7 @@ def loop():
         
         with open(csv_file,'a', newline='') as f:
             w = csv.writer(f)
-            w.writerow(np.insert(current_data, 0, time_getData)) #adding the current time stamp to the front of the csv line
+            w.writerow(np.concatenate(([str(time_getData)], current_data), axis = 0)) #adding the current time stamp to the front of the csv line
 
     
     fig.clf()
@@ -95,14 +92,14 @@ def loop():
     ax1.set_xlabel("Elapsed Time (Seconds)")
     ax1.set_ylabel("Fluorescence (RFU)")
     ax1.set_title("TARGET WAVELENGTH")
-    ax1.set_ylim([0,2000])
+    ax1.set_ylim([0,1030])
 
     ax2 = fig.add_subplot(1,2,2) 
     ax2.plot(wavelength, current_data)
     ax2.set_xlabel("Wavelength")
     ax2.set_ylabel("Intensity (UNIT)")
     ax2.set_title("INSTANT SPECTRUM")
-    ax2.set_ylim([-100, 2000]) # change or comment out if need (e.g. testing)
+    ax2.set_ylim([0, 1030]) # change or comment out if need (e.g. testing)
 
     canvas.draw()
     root.after(1,loop)
@@ -128,29 +125,49 @@ def calibration():
     #print calibration_set to csv for record 
 
 def new_sample():
-    global time_stamp, current_data, target_wavelength_data, sample_no 
+    global time_stamp, current_data, target_wavelength_data, sample_no, state_run, csv_file, current_sample
+
+    state_run = False
 
     target_wavelength_data = np.array([]) 
     time_stamp = np.array([])
     current_data = np.zeros(288,)
     sample_no += 1
+    csv_file = f"test{sample_no}.csv"
 
     with open(csv_file,'a', newline='') as f:
         w = csv.writer(f)
-        w.writerow()
+        w.writerow([f"sample no: {sample_no} "])
+    
+    current_sample.config(text=f"Current Sample No.:{sample_no}")
+
+    if (button_start["state"] ==  DISABLED):
+        button_start["state"] = NORMAL
+
+def gui_start():
+    global state_run, start_time, button_start
+    state_run = True
+    start_time = datetime.now()
+    button_start["state"] = DISABLED
+
+
 
 #widget
 current_sample = Label(text=f"Current Sample No.:{sample_no}")
 current_sample.pack(fill='x')
 
+"""
 button_calibration = Button(text = "CALIBRATION", command = calibration)
 button_calibration.pack()
+"""
 
 button_start = Button(text="START", command = gui_start)
 button_start.pack()
 
 button_new_sample = Button(text="NEW SAMPLE", command = new_sample)
 button_new_sample.pack()
+
+
 
 root.after(1,loop)
 root.mainloop()
